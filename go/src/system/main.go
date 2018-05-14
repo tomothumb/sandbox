fbe3049640a1e2dddafd6a292882ba04e5c48cba
Tomoyuki Tsujimoto <tomo.tsujimoto@gmail.com>
2018-05-14 23:26:27 +0900

    go: select2 sync

--- go/src/system/main.go
+++ go/src/system/main.go
@@ -11,6 +11,7 @@ import (
 	"time"
 	"net/http"
 	"context"
+	"sync"
 )
 
 // @link http://ascii.jp/elem/000/001/243/1243667/
@@ -182,6 +183,51 @@ func main()  {
 	}
 	fmt.Println("select finished")
 
+	// Select(sync)
+	fmt.Println("select1 start!")
+	myselect2_ch1 := select_fn2("2beer",4)
+	myselect2_ch2 := select_fn2("2juice",3)
+	myselect2_ch3 := select_fn2("2water",2)
+	var wg sync.WaitGroup
+	reporter := func(ch chan string){
+		defer wg.Done()
+		for str := range ch {
+			fmt.Println(str)
+		}
+	}
+	wg.Add(3)
+	go reporter(myselect2_ch1)
+	go reporter(myselect2_ch2)
+	go reporter(myselect2_ch3)
+	wg.Wait()
+	fmt.Println("select2 finish!")
+
+	// Select2(sync)
+	select3_wg.Add(3)
+	myselect3_ch1 := select_fn3("3beer",4)
+	myselect3_ch2 := select_fn3("3juice",3)
+	myselect3_ch3 := select_fn3("3water",2)
+	done := make(chan bool)
+	go func() {
+		select3_wg.Wait()
+		done <- true
+	}()
+	SELECT_LABEL3:
+	for {
+		select {
+		case str := <-myselect3_ch1:
+			fmt.Println(str)
+		case str := <-myselect3_ch2:
+			fmt.Println(str)
+		case str := <-myselect3_ch3:
+			fmt.Println(str)
+		case <-done:
+			break SELECT_LABEL3
+		}
+	}
+	fmt.Println("myselect3 Finished!")
+
+
 }
 
 
@@ -213,5 +259,29 @@ func select_fn(str string, n int) ( chan string, chan bool ) {
 		done <- true
 	}()
 	return ch, done
+}
+
+func select_fn2(str string, n int) (chan string){
+	ch := make(chan string)
+	go func() {
+		for i:=0; i<n; i++  {
+			time.Sleep(time.Second * 1)
+			ch <- str + " please!"
+		}
+		close(ch)
+	}()
+	return ch
+}
+
+var select3_wg sync.WaitGroup
+func select_fn3(str string, n int) (chan string)  {
+	ch := make(chan string)
+	go func() {
+		for i:=0; i<n; i++  {
+			ch <- str + " please!"
+		}
+		select3_wg.Done()
+	}()
+	return ch
 
 }
