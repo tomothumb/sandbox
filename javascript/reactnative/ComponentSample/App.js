b7d822f61a643a45993943e646a0213a542e7995
Tomoyuki Tsujimoto <tomo.tsujimoto@gmail.com>
2018-06-07 22:37:39 +0900

    reactnative component sample (GPS)

--- javascript/reactnative/ComponentSample/App.js
+++ javascript/reactnative/ComponentSample/App.js
@@ -2,13 +2,47 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 
+function getCurrentPosition(timeoutMillis = 10000) {
+    console.log(navigator.geolocation);
+    return new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject, { timeout: timeoutMillis });
+    });
+}
+
 
 export default class App extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state={
+            coords: null
+        }
+    }
+
+    async componentDidMount(){
+        try {
+            const position = await getCurrentPosition(5000);
+            const { latitude , longitude } = position.coords;
+            this.setState({
+                coords: {
+                    latitude,
+                    longitude
+                }
+            });
+        } catch (e) {
+            alert(e.message);
+        }
+    }
 
   render() {
     return (
       <View style={styles.container}>
           <Text>Welcome!</Text>
+          { this.state.coords ?
+              <Text>here: {this.state.coords.latitude}, {this.state.coords.longitude}</Text>
+              : null
+          }
+          <Text>Fiinish!</Text>
       </View>
     );
   }
