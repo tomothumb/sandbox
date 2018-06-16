bc529b4ccd261cc3ed8e2ee715452db07b04d478
Tomoyuki Tsujimoto <tomo.tsujimoto@gmail.com>
2018-06-17 03:07:53 +0900

    Swift: Create Project

---
+++ iOS/DemoApp/DemoApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DemoApp
+//
+//  Created by Tomoyuki Tsujimoto on 2018/06/17.
+//  Copyright © 2018年 Socialis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
