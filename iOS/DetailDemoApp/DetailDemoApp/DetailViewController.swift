42b11deb3c651fec8b6bdbec3554d3575baff227
Tomoyuki Tsujimoto <tomo.tsujimoto@gmail.com>
2018-06-17 22:06:50 +0900

    swift: demo app(ver detail)

---
+++ iOS/DetailDemoApp/DetailDemoApp/DetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DetailViewController.swift
+//  DetailDemoApp
+//
+//  Created by Tomoyuki Tsujimoto on 2018/06/17.
+//  Copyright © 2018年 Socialis. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
