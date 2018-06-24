e968b5b65ed9d7b8f19476d1da85eec1b341b550
Tomoyuki Tsujimoto <tomo.tsujimoto@gmail.com>
2018-06-24 19:37:10 +0900

    iOS: BookTutorial chapter1-4

--- iOS/BookTutorial/BookTutorial/ViewController.swift
+++ iOS/BookTutorial/BookTutorial/ViewController.swift
@@ -7,11 +7,22 @@
 //
 
 import UIKit
+import GameplayKit
 
 class ViewController: UIViewController {
+    
+    let randomSource = GKARC4RandomSource()
 
+    @IBOutlet weak var computerImageView: UIImageView!
+    @IBOutlet weak var playerImageView: UIImageView!
+    @IBOutlet weak var messageLabel: UILabel!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let angle:CGFloat = CGFloat((180.0 * Double.pi) / 180.0)
+        computerImageView.transform = CGAffineTransform(rotationAngle: angle)
+//        computerImageView.transform = computerImageView.transform.rotated(by: 60);
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +30,76 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func doComputer(player:Int){
+        let computer = randomSource.nextInt(upperBound: 3);
+        var msg = "";
+        switch computer {
+        case 0:
+            computerImageView.image = UIImage(named: "gu.png")
+            switch player {
+            case 0:
+                msg = "あいこ"
+            case 1:
+                msg = "Lose"
+            case 2:
+                msg = "Win"
+            default:
+                break
+            }
+        case 1:
+            computerImageView.image = UIImage(named: "choki.png")
+            switch player {
+            case 0:
+                msg = "Win"
+            case 1:
+                msg = "あいこ"
+            case 2:
+                msg = "Lose"
+            default:
+                break
+            }
 
+        case 2:
+            computerImageView.image = UIImage(named: "pa.png")
+            switch player {
+            case 0:
+                msg = "Lose"
+            case 1:
+                msg = "Win"
+            case 2:
+                msg = "あいこ"
+            default:
+                break
+            }
+        default:
+            break
+        }
+        messageLabel.text = msg;
+        
+        
+        
+    }
 
+    @IBAction func tapStart() {
+        computerImageView.image = UIImage(named: "gu.png")
+        playerImageView.image = UIImage(named: "gu.png")
+        messageLabel.text = "じゃんけん！"
+    }
+    @IBAction func tapGu() {
+        playerImageView.image = UIImage(named: "gu.png")
+        messageLabel.text = "グー！"
+        doComputer(player:0);
+    }
+    @IBAction func tapChoki() {
+        playerImageView.image = UIImage(named: "choki.png")
+        messageLabel.text = "チョキ！"
+        doComputer(player:1);
+    }
+    @IBAction func tapPa() {
+        playerImageView.image = UIImage(named: "pa.png")
+        messageLabel.text = "パー！"
+        doComputer(player:2);
+    }
 }
 
