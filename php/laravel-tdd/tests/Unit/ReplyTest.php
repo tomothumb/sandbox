c3e2f2d83de25456d62ddce5902b1eed4437f5ed
Tomoyuki Tsujimoto <tomo.tsujimoto@gmail.com>
2018-08-02 04:16:52 +0900

    php/tdd: reply <> user unit test

---
+++ php/laravel-tdd/tests/Unit/ReplyTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Unit;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ReplyTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function test_it_has_an_owner()
+    {
+        $reply = factory('App\Model\Reply')->create();
+
+        $this->assertInstanceOf('App\User', $reply->user);
+
+    }
+}
