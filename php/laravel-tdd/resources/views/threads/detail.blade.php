f980ad5734de5a4c083a4faebbaba4ee2b041c54
Tomoyuki Tsujimoto <tomo.tsujimoto@gmail.com>
2018-08-04 15:47:56 +0900

    php/tdd: add reply post form

--- php/laravel-tdd/resources/views/threads/detail.blade.php
+++ php/laravel-tdd/resources/views/threads/detail.blade.php
@@ -10,21 +10,45 @@
                         posted {{$thread->created_at->diffForHumans()}}<br>
                         {{$thread->title}}</div>
 
-
                     <div class="card-body">
                         {{$thread->body}}
                     </div>
-                    <hr>
+                </div>
 
+                @if(auth()->check())
+                <div class="card">
+                    <div class="card-header">
+                        <h3>Comment</h3>
+                    </div>
                     <div class="card-body">
-                        <h3>Reply</h3>
-                        @foreach( $thread->replies as $reply)
-                            @include( 'threads.reply')
-                        @endforeach
+                        <form method="POST" action="{{$thread->path() .'/replies' }}">
+                            <textarea name="body" id="body" rows="5" style="width: 100%;" placeholder="Have something to say?"></textarea>
+                            <p><input type="submit" value="POST"></p>
+                        </form>
                     </div>
                 </div>
+                    @else
+                    <p class="text-center">Please <a href="{{ route('login') }}">sign in</a> to participate in this discussion.</p>
+                @endif
+
+                <div class="card">
+                    <div class="card-header">
+                    <h3>Replies</h3>
+                    </div>
+                    <div class="card-body">
+                    @foreach( $thread->replies as $reply)
+                        @include( 'threads.reply')
+                    @endforeach
+                    </div>
+                </div>
+
+
+
             </div>
         </div>
+
+
+
     </div>
 @endsection
 
