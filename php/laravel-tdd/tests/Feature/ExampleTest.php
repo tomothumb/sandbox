fbcec8c998a0696a743ab725ff6dbdfe47a4919a
Tomoyuki Tsujimoto <tomo.tsujimoto@gmail.com>
2018-08-12 01:11:28 +0900

    php/tdd: test refactoring

--- php/laravel-tdd/tests/Feature/ExampleTest.php
+++ php/laravel-tdd/tests/Feature/ExampleTest.php
@@ -2,11 +2,15 @@
 
 namespace Tests\Feature;
 
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ExampleTest extends TestCase
 {
+
+    use DatabaseMigrations;
+
     /**
      * A basic test example.
      *
@@ -15,7 +19,6 @@ class ExampleTest extends TestCase
     public function testBasicTest()
     {
         $response = $this->get('/');
-
         $response->assertStatus(200);
     }
 }
