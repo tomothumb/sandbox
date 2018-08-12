ea9f612fc0101fe4fbd03c1dde325d40ded5baf9
Tomoyuki Tsujimoto <tomo.tsujimoto@gmail.com>
2018-08-13 03:17:30 +0900

    php/tdd: chow reply count

--- php/laravel-tdd/resources/views/threads/index.blade.php
+++ php/laravel-tdd/resources/views/threads/index.blade.php
@@ -5,8 +5,6 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
 
-
-
                 <div class="card">
                     <div class="card-header">Forum Threads</div>
 
@@ -17,13 +15,13 @@
                                     <a href="{{$thread->path()}}">
                                         {{$thread->id}}/
                                         {{$thread->title}}
-
                                     </a>
                                 </h4>
+                                <a href="{{$thread->path()}}" class="meta">
+                                    {{$thread->replies_count}} {{ str_plural('reply', $thread->replies_count) }}
+                                </a>
                                 <div class="body">{{$thread->body}}</div>
-
                             </article>
-
                             <hr>
                         @endforeach
                     </div>
