5b66249656276c3a127791440e1fdc7bd085519d
Tomoyuki Tsujimoto <tomo.tsujimoto@gmail.com>
2018-08-19 01:08:36 +0900

    php/tdd: clean view

--- php/laravel-tdd/resources/views/threads/index.blade.php
+++ php/laravel-tdd/resources/views/threads/index.blade.php
@@ -4,28 +4,26 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
+                <div>Forum Threads</div>
 
-                <div class="card">
-                    <div class="card-header">Forum Threads</div>
+                @foreach($threads as $thread)
 
-                    <div class="card-body">
-                        @foreach($threads as $thread)
-                            <article>
-                                <h4>
-                                    <a href="{{$thread->path()}}">
-                                        {{$thread->id}}/
-                                        {{$thread->title}}
-                                    </a>
-                                </h4>
-                                <a href="{{$thread->path()}}" class="meta">
-                                    {{$thread->replies_count}} {{ str_plural('reply', $thread->replies_count) }}
+                    <div class="card">
+
+                        <div class="card-body">
+                            <h4>
+                                <a href="{{$thread->path()}}">
+                                    {{$thread->id}}/
+                                    {{$thread->title}}
                                 </a>
-                                <div class="body">{{$thread->body}}</div>
-                            </article>
-                            <hr>
-                        @endforeach
+                            </h4>
+                            <a href="{{$thread->path()}}" class="meta">
+                                {{$thread->replies_count}} {{ str_plural('reply', $thread->replies_count) }}
+                            </a>
+                            <div class="body">{{$thread->body}}</div>
+                        </div>
                     </div>
-                </div>
+                @endforeach
             </div>
         </div>
     </div>
