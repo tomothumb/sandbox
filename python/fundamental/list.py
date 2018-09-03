cea1a1cbfd17789c1ca78a579c66dfafe61a5b19
Tomoyuki Tsujimoto <tomo.tsujimoto@gmail.com>
2018-09-03 21:51:16 +0900

    Python: fundamental list

---
+++ python/fundamental/list.py
@@ -0,0 +1,33 @@
+
+l = [1,2,3,4,60,3]
+print(l)
+print(l[0])
+print(l[4])
+print(l[-3])
+
+print(l[2:5])
+print(l[:2])
+print(l[2:])
+print(l[:])
+
+print(len(l))
+print(type(l))
+print("#####")
+print(list("abcdefg"))
+print("#####")
+print("#####")
+
+l2 = [1,2,3,4,5,6,7,8,9,10]
+print(l2[::2])
+print(l2[::3])
+print(l2[::-1])
+
+print("#####")
+
+str_list = ['a','b','c']
+num_list = [1,2,3]
+combined_list = [str_list,num_list]
+print(combined_list)
+print(combined_list[0])
+print(combined_list[0][1])
+print(combined_list[1][2])
