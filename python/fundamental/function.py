4342546dff03a1fe4d32fa4adb4ec218dfda01c1
Tomoyuki Tsujimoto <tomo.tsujimoto@gmail.com>
2018-09-16 01:17:50 +0900

    Python: fundamental function2 inner function closure

--- python/fundamental/function.py
+++ python/fundamental/function.py
@@ -72,26 +72,29 @@ menu('chicken', drink='beer')
 
 print('########')
 
-def test_func(x, l=[]): # バグにつながる可能性
+
+def test_func(x, l=[]):  # バグにつながる可能性
     l.append(x)
     return l
 
-y  = [1,2,3]
-r = test_func(100,y)
+
+y = [1, 2, 3]
+r = test_func(100, y)
 print(r)
 
-y  = [1,2,3]
-r = test_func(200,y)
+y = [1, 2, 3]
+r = test_func(200, y)
 print(r)
 print('########')
 
 r = test_func(100)
 print(r)
 
-r = test_func(100) # バグにつながる可能性
+r = test_func(100)  # バグにつながる可能性
 print(r)
 
 print('########')
+
 
 # バグ解決版。Noneを指定してNoneの場合にデフォルトを指定する
 
@@ -101,6 +104,7 @@ def test_func(x, l=None):
     l.append(x)
     return l
 
+
 print('########')
 r = test_func(100)
 print(r)
@@ -109,20 +113,120 @@ r = test_func(100)
 print(r)
 
 print('########')
+
 
 def say_something(word, word2, word3):
     print(word, word2, word3)
 
+
 r = say_something('Hi!', 'Mike', 'Nance')
 print('########')
+
+
 def say_something(word, *args):
     for arg in args:
         print(arg)
-    print("word  : ",word)
+    print("word  : ", word)
+
 
 print('########')
 r = say_something('Hi!', 'Mike', 'Nance')
 print('########')
 t = ('Mike', 'Nancy')
 r = say_something('Hi!', *t)
+
+print('########')
+
+
+def menu(entree='beef', drink='wine'):
+    print(entree, drink)
+
+
+menu(entree='beef', drink='coffee')
+
+
+def menu(**kwargs):
+    print(kwargs)
+    for k, v in kwargs.items():
+        print(k, v)
+
+
+menu(entree='beef', drink='coffee')
+
+d = {
+    'entree': 'beef',
+    'drink': 'ice coffee',
+    'dessert': 'ice'
+}
+menu(**d)
+
+print('########')
+
+
+def menu2(food, *args, **kwargs):
+    print(food)
+    print(args)
+    print(kwargs)
+
+
+menu2('banana', 'apple', 'orange', entree='beef', drink='coffee')
+
+print('########')
+
+
+def example_func(param1, param2):
+    """
+    demodemodemo
+
+    :param param1:
+    :param param2:
+    :return:
+    """
+
+    print(param1)
+    print(param2)
+    return True
+
+
+print(example_func.__doc__)
+help(example_func)
+
+print('########')
+def outer(a, b):
+    print(a, b)
+
+    def plus(c, d):
+        return c + d
+
+    r1 = plus(a, b)
+    r2 = plus(b, a)
+    print(r1+r2)
+
+outer(1, 2)
+
+print('########')
+# closure
+def outer(a,b):
+    def inner():
+        return a+b
+    return inner
+
+f = outer(1,2)
+r = f()
+print(r)
+
+print('########')
+# closure example
+def circle_area_func(pi):
+    def circle_area(radius):
+        return pi * radius * radius
+    return circle_area
+
+ca1 = circle_area_func(3.14)
+ca2 = circle_area_func(3)
+ca3 = circle_area_func(3.1415)
+
+print(ca1(10))
+print(ca2(10))
+print(ca3(10))
 
