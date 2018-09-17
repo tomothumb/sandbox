5b4dcf0e67dd837ef01b1660c473b09b558457a6
Tomoyuki Tsujimoto <tomo.tsujimoto@gmail.com>
2018-09-17 16:09:58 +0900

    Python: fundamental function4 lambda

--- python/fundamental/function.py
+++ python/fundamental/function.py
@@ -312,3 +312,24 @@ print(r)
 print('#####')
 r = divide_num(10, 20)
 print(r)
+
+print('########')
+# lambda
+l = ['Mon', 'tue', 'Wed', 'Thu', 'fri', 'sat', 'Sun']
+
+
+def change_words(words, func):
+    for word in words:
+        print(func(word))
+
+
+# def sample_func(word):
+#     return word.capitalize()
+# def sample_func2(word):
+#     return word.lower()
+
+sample_func = lambda word: word.capitalize()
+
+change_words(l, sample_func)
+change_words(l, lambda word: word.capitalize())
+change_words(l, lambda word: word.lower())
