92a0ce297bb3e53fc72d606d03baa2ffbb0373b4
Tomoyuki Tsujimoto <tomo.tsujimoto@gmail.com>
2018-09-17 16:01:06 +0900

    Python: fundamental function3 decorator

--- python/fundamental/function.py
+++ python/fundamental/function.py
@@ -192,6 +192,8 @@ print(example_func.__doc__)
 help(example_func)
 
 print('########')
+
+
 def outer(a, b):
     print(a, b)
 
@@ -200,27 +202,36 @@ def outer(a, b):
 
     r1 = plus(a, b)
     r2 = plus(b, a)
-    print(r1+r2)
+    print(r1 + r2)
+
 
 outer(1, 2)
 
 print('########')
+
+
 # closure
-def outer(a,b):
+def outer(a, b):
     def inner():
-        return a+b
+        return a + b
+
     return inner
 
-f = outer(1,2)
+
+f = outer(1, 2)
 r = f()
 print(r)
 
 print('########')
+
+
 # closure example
 def circle_area_func(pi):
     def circle_area(radius):
         return pi * radius * radius
+
     return circle_area
+
 
 ca1 = circle_area_func(3.14)
 ca2 = circle_area_func(3)
@@ -230,3 +241,74 @@ print(ca1(10))
 print(ca2(10))
 print(ca3(10))
 
+print('########')
+
+
+# decorator
+def add_num(a, b):
+    return a + b
+
+
+print('start')
+r = add_num(10, 20)
+print('edn')
+print(r)
+
+print('#####')
+
+
+def print_info(func):
+    def wrapper(*args, **kwargs):
+        print('start')
+        result = func(*args, **kwargs)
+        print('end')
+        return result
+
+    return wrapper
+
+
+def print_more(func):
+    def wrapper(*args, **kwargs):
+        print('func:', func.__name__)
+        print('args:', args)
+        print('kwargs:', kwargs)
+        result = func(*args, **kwargs)
+        print('result:', result)
+        print('end')
+        return result
+
+    return wrapper
+
+
+f = print_info(add_num)
+r = f(10, 20)
+print(r)
+
+print('#####')
+
+
+@print_info
+def sub_num(a, b):
+    return a - b
+
+
+@print_info
+@print_more
+def multi_num(a, b):
+    return a * b
+
+
+@print_more
+@print_info
+def divide_num(a, b):
+    return a % b
+
+
+r = sub_num(10, 20)
+print(r)
+print('#####')
+r = multi_num(10, 20)
+print(r)
+print('#####')
+r = divide_num(10, 20)
+print(r)
