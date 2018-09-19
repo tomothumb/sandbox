84e5696b62769f48d4ed4f5bcc0466046406cd7c
Tomoyuki Tsujimoto <tomo.tsujimoto@gmail.com>
2018-09-20 00:46:35 +0900

    Python: fundamental packaging
    python setup.py sdist

---
+++ python/fundamental/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='fundamental',
+    version='0.0.1',
+    packages=['sample_package',
+              'sample_package.talk', 'sample_package.tools'],
+    url='',
+    license='mit',
+    author='tomo',
+    author_email='',
+    description=''
+)
+
+# python setup.py sdist
