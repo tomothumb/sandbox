6e69168f10d61f54f12d66a0526e775764f5de31
Tomoyuki Tsujimoto <tomo.tsujimoto@gmail.com>
2018-09-21 20:19:59 +0900

    Python: fundamental class ( extends / method override / super() )

--- python/fundamental/class.py
+++ python/fundamental/class.py
@@ -13,7 +13,7 @@ class Person(object):
 
     # destructor
     def __del__(self):
-        print('good bye')
+        print('good bye "Person Class"')
 
 
 print("########")
@@ -24,3 +24,54 @@ print("########")
 person2 = Person()
 person2.say_something()
 print("########")
+
+
+class Car(object):
+    def __init__(self, model=None):
+        self.model = model
+
+
+    def run(self):
+        print('run')
+
+
+class ToyotaCar(Car):
+    pass
+
+
+class NissanCar(Car):
+    def run(self):
+        print('fast')
+
+
+class TeslaCar(Car):
+    def __init__(self, model='Model S', enable_auto_run=False):
+        # self.model = model
+        super().__init__(model)
+        self.enable_auto_run = enable_auto_run
+    def run(self):
+        print('super fast')
+
+    def auto_run(self):
+        print('auto run')
+
+
+print("########")
+car = Car()
+car.run()
+
+print("########")
+toyota_car = ToyotaCar('Lexus')
+toyota_car.run()
+print(toyota_car.model)
+
+print("########")
+nissan_car = NissanCar('Leaf')
+nissan_car.run()
+print(nissan_car.model)
+
+print("########")
+tesla_car = TeslaCar('Model S')
+tesla_car.run()
+tesla_car.auto_run()
+print(tesla_car.model)
