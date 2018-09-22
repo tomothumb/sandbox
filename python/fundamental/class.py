f59b662ab5a48c654da890dfd26a3ca16ac5a6ce
Tomoyuki Tsujimoto <tomo.tsujimoto@gmail.com>
2018-09-22 15:23:45 +0900

    Python: fundamental class ( private / getter / setter )

--- python/fundamental/class.py
+++ python/fundamental/class.py
@@ -45,10 +45,34 @@ class NissanCar(Car):
 
 
 class TeslaCar(Car):
-    def __init__(self, model='Model S', enable_auto_run=False):
+    def __init__(self, model='Model S', enable_auto_run=False, passwd='123'):
         # self.model = model
         super().__init__(model)
         self.enable_auto_run = enable_auto_run
+        self.passwd = passwd
+
+        # Priavate var
+        self.__private_val = True
+        self.__private_val2 = True
+
+    # GETTER
+    @property
+    def private_val(self):
+        return self.__private_val
+
+    @property
+    def private_val2(self):
+        return self.__private_val2
+
+    # SETTER
+    @private_val2.setter
+    def private_val2(self, v):
+        if self.passwd == '456':
+            self.__private_val2 = v
+        else:
+            raise ValueError
+
+
     def run(self):
         print('super fast')
 
@@ -75,3 +99,15 @@ tesla_car = TeslaCar('Model S')
 tesla_car.run()
 tesla_car.auto_run()
 print(tesla_car.model)
+print(tesla_car.private_val)
+
+print('tesla_car:',tesla_car.private_val2)
+#tesla_car.private_val2 = False
+#print('tesla_car:',tesla_car.private_val2)
+
+print("########")
+
+tesla_car2 = TeslaCar('Model M', passwd='456')
+tesla_car2.private_val2 = False
+print('tesla_car2:',tesla_car2.private_val2)
+
