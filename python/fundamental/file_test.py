32293032480b1a20c0032dcfd581336a8ed98a93
Tomoyuki Tsujimoto <tomo.tsujimoto@gmail.com>
2018-09-24 17:21:34 +0900

    Python: fundamental file io (seek / write and read)

--- python/fundamental/file_test.py
+++ python/fundamental/file_test.py
@@ -1,21 +1,28 @@
 # Write
-f = open('file_test.txt','w')
+f = open('file_test.txt', 'w')
 f.write('Test\n')
-print('My','name','is', 'Mike', sep='#', end='!\n', file=f)
+print('My', 'name', 'is', 'Mike', sep='#', end='!\n', file=f)
 # f.write()
 f.close()
 
 print("########")
+# Write and Read
+with open('file_test.txt', 'w+') as myfile_wplus:
+    myfile_wplus.write('w+open\n')
+    myfile_wplus.seek(0)
+    print(myfile_wplus.read())
+
+print("########")
 # Append
-f = open('file_test.txt','a')
+f = open('file_test.txt', 'a')
 f.write('Test\n')
 f.close()
 
 print("########")
 # with statement
-with open('file_test.txt','a') as myfile:
+with open('file_test.txt', 'a') as myfile:
     myfile.write('with open write\n')
-    print('My','name','is', 'Mike', sep='_', end='!\n', file=myfile)
+    print('My', 'name', 'is', 'Mike', sep='_', end='!\n', file=myfile)
 
 print("########")
 # Read
@@ -41,3 +48,20 @@ with open('file_test.txt', 'r') as myfile_chunk:
         print(s, end='|')
         if not s:
             break
+
+print("########")
+# Seek
+with open('file_test.txt', 'r') as myfile_seek:
+    print(myfile_seek.tell())
+    print(myfile_seek.read(3))
+    myfile_seek.seek(5)
+    print(myfile_seek.tell())
+    print(myfile_seek.read(3))
+    print(myfile_seek.tell())
+    myfile_seek.seek(14)
+    print(myfile_seek.tell())
+    print(myfile_seek.read(3))
+    myfile_seek.seek(999)
+    print(myfile_seek.read(3))
+    myfile_seek.seek(5)
+    print(myfile_seek.read(3))
