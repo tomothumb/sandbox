8b214e1d58fbbe969caabe8ab7bd21fc2fb04561
Tomoyuki Tsujimoto <tomo.tsujimoto@gmail.com>
2018-09-24 16:55:22 +0900

    Python: fundamental class (magic method)

--- python/fundamental/class.py
+++ python/fundamental/class.py
@@ -253,3 +253,31 @@ print('Woman.kind:',Woman.kind)
 print('Woman.what_is_your_kind:',Woman.what_is_your_kind())
 Woman.about(1000)
 
+print("########")
+# magic methods
+class Word(object):
+
+    def __init__(self, text):
+        self.text = text
+
+    def __str__(self):
+        return 'Word!!!!!'
+
+    def __len__(self):
+        return len(self.text)
+
+    def __add__(self, word):
+        return self.text.lower() + word.text.lower()
+
+    def __eq__(self, word):
+        return self.text.lower() == word.text.lower()
+
+w = Word('test')
+w2 = Word('+++++ ')
+w3 = Word('test')
+print(w)
+print(len(w))
+print(w.text + w2.text)
+print(w + w2)
+print(w == w2)
+print(w == w3)
