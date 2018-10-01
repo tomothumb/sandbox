47b0eeaf0d4c92f159a4823cf800123233774a92
Tomoyuki Tsujimoto <tomo.tsujimoto@gmail.com>
2018-10-01 22:40:05 +0900

    Python: deep-learning-from-scratch chapter4 (cross entropy error with mini batch )

--- python/deep-learning-from-scratch/ch04/main.py
+++ python/deep-learning-from-scratch/ch04/main.py
@@ -7,8 +7,15 @@ def mean_squared_error(y, t):
     return 0.5 * np.sum((y - t) ** 2)
 
 def cross_entropy_error(y, t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+
+    batch_size = y.shape[0]
     delta = 1e-7
-    return -np.sum(t * np.log(y +delta))
+    # return -np.sum(t * np.log(y + delta)) / batch_size
+    return -np.sum(np.log(y[np.arange(batch_size), t] + delta)) / batch_size
+
 
 
 t = [0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
