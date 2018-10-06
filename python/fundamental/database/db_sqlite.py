6255b90e7c72fba421353feb445cdad4f4a680f5
Tomoyuki Tsujimoto <tomo.tsujimoto@gmail.com>
2018-10-07 00:31:30 +0900

    Python: fundamental database (sqlite3) 2

--- python/fundamental/database/db_sqlite.py
+++ python/fundamental/database/db_sqlite.py
@@ -1,22 +1,21 @@
 import sqlite3
 
-conn = sqlite3.connect('./database/db_sqlite.sqlite')
+# conn = sqlite3.connect('./database/db_sqlite.sqlite')
+conn = sqlite3.connect(':memory:')
 
 curs = conn.cursor()
-# curs.execute(
-#     'CREATE TABLE persons(id INTEGER PRIMARY KEY AUTOINCREMENT, name STRING)'
-# )
-# conn.commit()
-#
-# curs.execute(
-#     'INSERT INTO persons(name) values("Mike")'
-# )
-# conn.commit()
+curs.execute(
+    'CREATE TABLE persons(id INTEGER PRIMARY KEY AUTOINCREMENT, name STRING)'
+)
+conn.commit()
+
+curs.execute(
+    'INSERT INTO persons(name) values("Mike")'
+)
+conn.commit()
 
 print("########")
-curs.execute(
-    'SELECT * FROM persons'
-)
+curs.execute('SELECT * FROM persons')
 print(curs.fetchall())
 
 print("########")
@@ -26,14 +25,25 @@ curs.execute(
 curs.execute(
     'INSERT INTO persons(name) values("Jun")'
 )
-print(curs.fetchall())
 conn.commit()
+curs.execute('SELECT * FROM persons')
+print(curs.fetchall())
 
+print("########")
 curs.execute(
     'UPDATE persons set name = "Michel" WHERE name = "Mike"'
 )
-print(curs.fetchall())
 conn.commit()
+curs.execute('SELECT * FROM persons')
+print(curs.fetchall())
 
+print("########")
+curs.execute(
+    'DELETE FROM persons WHERE name = "Michel"'
+)
+conn.commit()
+curs.execute('SELECT * FROM persons')
+print(curs.fetchall())
 
+curs.close()
 conn.close()
