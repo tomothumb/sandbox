a9902c6c1b73cc41b1344d36f1aaa76e2256849f
Tomoyuki Tsujimoto <tomo.tsujimoto@gmail.com>
2018-11-08 04:26:10 +0900

    ruby/rails: tutorial 11.1 preparing to imprement user activation

--- ruby/rails/RoRtutorial/app/models/user.rb
+++ ruby/rails/RoRtutorial/app/models/user.rb
@@ -1,12 +1,10 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
+
+  before_save :downcase_email
+  before_create :create_activation_digest
 
   has_many :microposts
-
-  before_save do
-    # self.email = email.downcase
-    email.downcase!
-  end
 
   validates :name, presence:true, length: {maximum: 50}
 
@@ -45,6 +43,16 @@ class User < ApplicationRecord
   def User.new_token
     SecureRandom.urlsafe_base64
   end
+
+  private
+    def downcase_email
+      self.email = email.downcase
+    end
+
+  def create_activation_digest
+      self.activation_token = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 
 
 end
