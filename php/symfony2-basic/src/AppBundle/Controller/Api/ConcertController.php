080592a89b97701ed6b6be500df4b0bc8684e88a
Tomoyuki Tsujimoto <tomo.tsujimoto@gmail.com>
2018-11-17 14:59:37 +0900

    php/symfony2: api doc (nelmio/api-doc-bundle)

--- php/symfony2-basic/src/AppBundle/Controller/Api/ConcertController.php
+++ php/symfony2-basic/src/AppBundle/Controller/Api/ConcertController.php
@@ -5,10 +5,16 @@ namespace AppBundle\Controller\Api;
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Controller\Annotations as REST;
 use FOS\RestBundle\View\View;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 
 class ConcertController extends FOSRestController
 {
 
+    /**
+     * @ApiDoc(
+     *     description="公演情報の一覧を取得します"
+     * )
+     */
     public function getConcertsAction()
     {
         $em = $this->get('doctrine')->getManager();
