f3530a5a1e2610d702134da6b5b4035aaf251956
Tomoyuki Tsujimoto <tomo.tsujimoto@gmail.com>
2018-11-18 21:05:30 +0900

    php/symfony2: create first service

---
+++ php/symfony2-basic/src/AppBundle/Service/Csv/InquiryCsvBuilder.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace AppBundle\Service\Csv;
+
+use AppBundle\Entity\Inquiry;
+use League\Csv\Writer;
+
+class InquiryCsvBuilder
+{
+    private $encoding;
+    private $inquiryRepository;
+
+    /**
+     * InquiryCsvBuilder constructor.
+     * @param $encoding
+     * @param $inquiryRepository
+     */
+    public function __construct($encoding, $inquiryRepository)
+    {
+        $this->encoding = $encoding;
+        $this->inquiryRepository = $inquiryRepository;
+    }
+
+    /**
+     * CSV形式の文字列を作成する
+     * @param $keyword
+     * @return null|string|string[]
+     * @throws \League\Csv\CannotInsertRecord
+     */
+    public function build($keyword)
+    {
+        $inquiryList = $this->inquiryRepository->findAllByKeyword($keyword);
+
+        /** @var Writer $writer */
+        $writer = Writer::createFromString('','');
+        $writer->setNewline("\r\n");
+
+        foreach ($inquiryList as $inquiry) {
+            /** @var Inquiry $inquiry */
+            $writer->insertOne([
+                $inquiry->getId(),
+                $inquiry->getName(),
+                $inquiry->getEmail()
+            ]);
+        }
+
+        return mb_convert_encoding((string) $writer, $this->encoding, 'UTF-8');
+    }
+
+}
