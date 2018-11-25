4922f52eea08a97bf61ba02244e1a7bd56e6e861
Tomoyuki Tsujimoto <tomo.tsujimoto@gmail.com>
2018-11-25 23:50:43 +0900

    php/symfony3.4: simple routing

---
+++ php/symfony3-basic/src/AppBundle/Controller/GenusController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace AppBundle\Controller;
+
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class GenusController extends Controller
+{
+
+    /**
+     * @Route("/genus/{genusName}")
+     */
+    public function showAction($genusName)
+    {
+        return new Response("The genus: ". $genusName);
+    }
+
+
+
+}
