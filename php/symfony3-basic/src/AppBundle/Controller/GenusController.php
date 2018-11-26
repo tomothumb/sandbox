94bf92df7f1c71e456d227826864215979eafd4a
Tomoyuki Tsujimoto <tomo.tsujimoto@gmail.com>
2018-11-26 22:54:11 +0900

    php/symfony3.4: basic twig/controller/jsonResponse

--- php/symfony3-basic/src/AppBundle/Controller/GenusController.php
+++ php/symfony3-basic/src/AppBundle/Controller/GenusController.php
@@ -3,8 +3,10 @@
 namespace AppBundle\Controller;
 
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 class GenusController extends Controller
@@ -15,9 +17,33 @@ class GenusController extends Controller
      */
     public function showAction($genusName)
     {
-        return new Response("The genus: ". $genusName);
+        return $this->render('genus/show.html.twig', [
+            'name' => $genusName,
+            'notes' => [
+                1,
+                2,
+                3,
+                10
+            ]
+        ]);
     }
 
+    /**
+     * @Route("/genus/{genusName}/notes", name="genus_show_notes")
+     * @Method("GET")
+     */
+    public function getNotesAction()
+    {
+        $notes = [
+            ['id' => 1, 'username' => 'foo'],
+            ['id' => 2, 'username' => 'bar'],
+        ];
+        $data = [
+            'notes' => $notes
+        ];
+//        return new Response(json_encode($data));
+        return new JsonResponse($data);
+    }
 
 
 }
