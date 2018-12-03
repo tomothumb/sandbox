b089d4afbc5b822a597a4e40ccb0bca009b75192
Tomoyuki Tsujimoto <tomo.tsujimoto@gmail.com>
2018-12-03 23:13:05 +0900

    Laravel: twilio sample long sentense.

--- php/laravel-notification/app/Http/Controllers/TwilioController.php
+++ php/laravel-notification/app/Http/Controllers/TwilioController.php
@@ -22,7 +22,8 @@ class TwilioController extends Controller
 
     public function sendSMSByDI($phone_number, SmsSendable $sms_service)
     {
-        $body_message = 'こんにちは! This is TwilioServiceのFacadeデモ。DI Provider';
+        $body_message = 'こんにちは! This is TwilioServiceのFacadeデモ。DI Provider  https://google.com/';
+        $body_message = 'あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほあいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほあいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほ  https://google.com/';
         $sms_service->sendSMS($phone_number, $body_message);
         return redirect('/twilio/');
     }
