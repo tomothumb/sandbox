cf12092f3f91cd399fe0e4a5ee44b38c9ca3582d
Tomoyuki Tsujimoto <tomo.tsujimoto@gmail.com>
2019-01-04 21:00:06 +0900

    Laravel: Int to IPv4 util

--- php/laravel-samples/app/Service/Whois/IpUtil.php
+++ php/laravel-samples/app/Service/Whois/IpUtil.php
@@ -8,11 +8,20 @@ class IpUtil
     {
         $ip_sep = explode(".", $ip);
         $number
-            = (int) $ip_sep[0] * 1000 * 1000 * 1000
-            + (int) $ip_sep[1] * 1000 * 1000
-            + (int) $ip_sep[2] * 1000
-            + (int) $ip_sep[3]
-        ;
+            = (int)$ip_sep[0] * 1000 * 1000 * 1000
+            + (int)$ip_sep[1] * 1000 * 1000
+            + (int)$ip_sep[2] * 1000
+            + (int)$ip_sep[3];
         return $number;
+    }
+
+    public static function intToIpv4($number)
+    {
+        $ipv4 = ((int)substr($number, 0, -9)) . "." .
+            ((int)substr($number, -9, 3)) . "." .
+            ((int)substr($number, -6, 3)) . "." .
+            ((int)substr($number, -3, 3));
+        return $ipv4;
+
     }
 }
--- php/laravel-samples/tests/Unit/IpUtilTest.php
+++ php/laravel-samples/tests/Unit/IpUtilTest.php
@@ -10,11 +10,18 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 class IpUtilTest extends TestCase
 {
 
-    public function testExample()
+    public function testIpv4ToInt()
     {
         $this->assertSame(11022033044,IpUtil::ipv4ToInt("11.22.33.44"));
         $this->assertSame(1002003004,IpUtil::ipv4ToInt("1.2.3.4"));
         $this->assertSame(127000000001,IpUtil::ipv4ToInt("127.0.0.1"));
         $this->assertSame(255255255255,IpUtil::ipv4ToInt("255.255.255.255"));
+    }
+    public function testIntToIpv4()
+    {
+        $this->assertSame("11.22.33.44",IpUtil::intToIpv4(11022033044));
+        $this->assertSame("1.2.3.4",IpUtil::intToIpv4(1002003004));
+        $this->assertSame("127.0.0.1",IpUtil::intToIpv4(127000000001));
+        $this->assertSame("255.255.255.255",IpUtil::intToIpv4(255255255255));
     }
 }
