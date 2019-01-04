7f7028ae5a260f0891822b5eec76d64191f97357
Tomoyuki Tsujimoto <tomo.tsujimoto@gmail.com>
2019-01-05 00:08:26 +0900

    Laravel: add IPv4 increment util method

--- php/laravel-samples/app/Service/Whois/IpUtil.php
+++ php/laravel-samples/app/Service/Whois/IpUtil.php
@@ -22,6 +22,38 @@ class IpUtil
             ((int)substr($number, -6, 3)) . "." .
             ((int)substr($number, -3, 3));
         return $ipv4;
+    }
 
+    /**
+     * @param string $ip 11.22.33.44
+     * @return string 11.22.33.45
+     */
+    public static function increment(string $ip)
+    {
+        $ip_sep = explode(".", $ip);
+        $nextip_sep = [
+            (int) $ip_sep[0],
+            (int) $ip_sep[1],
+            (int) $ip_sep[2],
+            (int) $ip_sep[3] + 1
+        ];
+        
+        if($nextip_sep[3] >= 256 ){
+            $nextip_sep[3] = 0;
+            $nextip_sep[2] += 1;
+            if($nextip_sep[2] >= 256 ){
+                $nextip_sep[2] = 0;
+                $nextip_sep[1] += 1;
+                if($nextip_sep[1] >= 256 ){
+                    $nextip_sep[1] = 0;
+                    $nextip_sep[0] += 1;
+                }
+            }
+        }
+        if($nextip_sep[0]>=256){
+            $nextip_sep[0] = 1;
+        }
+
+        return implode(".",$nextip_sep);
     }
 }
--- php/laravel-samples/tests/Unit/IpUtilTest.php
+++ php/laravel-samples/tests/Unit/IpUtilTest.php
@@ -4,24 +4,34 @@ namespace Tests\Unit;
 
 use App\Service\Whois\IpUtil;
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class IpUtilTest extends TestCase
 {
 
     public function testIpv4ToInt()
     {
-        $this->assertSame(11022033044,IpUtil::ipv4ToInt("11.22.33.44"));
-        $this->assertSame(1002003004,IpUtil::ipv4ToInt("1.2.3.4"));
-        $this->assertSame(127000000001,IpUtil::ipv4ToInt("127.0.0.1"));
-        $this->assertSame(255255255255,IpUtil::ipv4ToInt("255.255.255.255"));
+        $this->assertSame(11022033044, IpUtil::ipv4ToInt("11.22.33.44"));
+        $this->assertSame(1002003004, IpUtil::ipv4ToInt("1.2.3.4"));
+        $this->assertSame(127000000001, IpUtil::ipv4ToInt("127.0.0.1"));
+        $this->assertSame(255255255255, IpUtil::ipv4ToInt("255.255.255.255"));
     }
+
     public function testIntToIpv4()
     {
-        $this->assertSame("11.22.33.44",IpUtil::intToIpv4(11022033044));
-        $this->assertSame("1.2.3.4",IpUtil::intToIpv4(1002003004));
-        $this->assertSame("127.0.0.1",IpUtil::intToIpv4(127000000001));
-        $this->assertSame("255.255.255.255",IpUtil::intToIpv4(255255255255));
+        $this->assertSame("11.22.33.44", IpUtil::intToIpv4(11022033044));
+        $this->assertSame("1.2.3.4", IpUtil::intToIpv4(1002003004));
+        $this->assertSame("127.0.0.1", IpUtil::intToIpv4(127000000001));
+        $this->assertSame("255.255.255.255", IpUtil::intToIpv4(255255255255));
+    }
+
+    public function testIncrementIp()
+    {
+        $this->assertSame("1.1.1.2", IpUtil::increment("1.1.1.1"));
+        $this->assertSame("1.1.1.10", IpUtil::increment("1.1.1.9"));
+        $this->assertSame("1.1.2.0", IpUtil::increment("1.1.1.255"));
+        $this->assertSame("1.2.0.0", IpUtil::increment("1.1.255.255"));
+        $this->assertSame("2.0.0.0", IpUtil::increment("1.255.255.255"));
+        $this->assertSame("2.0.0.1", IpUtil::increment("2.0.0.0"));
+        $this->assertSame("10.0.0.0", IpUtil::increment("9.255.255.255"));
     }
 }
