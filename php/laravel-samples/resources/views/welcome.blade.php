7ce8819cc4ba3e02e551b4331eac133997bcf7b6
Tomoyuki Tsujimoto <tomo.tsujimoto@gmail.com>
2019-01-07 04:05:08 +0900

    Laravel/sample: laravel file manager

--- php/laravel-samples/resources/views/welcome.blade.php
+++ php/laravel-samples/resources/views/welcome.blade.php
@@ -84,6 +84,10 @@
                 </li>
             @endforeach
         </ul>
+
+        <h3>File Manager</h3>
+        <iframe src="/laravel-filemanager" style="width: 100%; height: 500px; overflow: hidden; border: none;"></iframe>
+
     </div>
 </div>
 </body>
