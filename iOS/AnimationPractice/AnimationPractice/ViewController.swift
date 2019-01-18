91cbeb910da310d02a94fbd1be18a32ffc0a2c54
Tomoyuki Tsujimoto <tomo.tsujimoto@gmail.com>
2019-01-18 19:07:24 +0900

    iOS/AniamtionPractice: init

---
+++ iOS/AnimationPractice/AnimationPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimationPractice
+//
+//  Created by Tomoyuki Tsujimoto on 2019/01/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
