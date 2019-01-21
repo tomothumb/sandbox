43205147c08aaf5a1cff35fee451a1ddc8b6ae37
Tomoyuki Tsujimoto <tomo.tsujimoto@gmail.com>
2019-01-21 18:09:06 +0900

    ios: autolayout constraint anchor sample

--- iOS/AnimationPractice/AnimationPractice/MiscController.swift
+++ iOS/AnimationPractice/AnimationPractice/MiscController.swift
@@ -17,6 +17,32 @@ class MiscController: UIViewController{
         super.viewDidLoad()
         navigationItem.title = "Other"
         
+        
+        //ボックスレイアウト
+        let purpleView = UIView()
+        purpleView.backgroundColor = .purple
+        view.addSubview(purpleView)
+        purpleView.fillSuperview()
+        
+        let redView = UIView()
+        let blueView = UIView()
+        let greenView = UIView()
+        redView.backgroundColor = .red
+        blueView.backgroundColor = .blue
+        greenView.backgroundColor = .green
+        [redView,blueView,greenView].forEach { (subview) in
+            view.addSubview(subview)
+        }
+        
+        redView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: nil, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 10, left: 0, bottom: 0, right: 12), size: .init(width: 100, height: 0))
+        redView.heightAnchor.constraint(equalTo: redView.widthAnchor).isActive = true
+        
+        blueView.anchor(top: redView.bottomAnchor, leading: nil, bottom: nil, trailing: redView.trailingAnchor, padding: .init(top: 10, left: 0, bottom: 0, right: 0))
+        blueView.anchorSize(to: redView)
+        
+        greenView.anchor(top: redView.topAnchor, leading: view.safeAreaLayoutGuide.leadingAnchor, bottom: blueView.bottomAnchor, trailing: redView.leadingAnchor, padding: .init(top: 0, left: 12, bottom: 0, right: 12))
+    
+        
         // Textbox
         let textView = UITextView()
         textView.frame = CGRect(x: 0, y: 0, width: 200, height: 100)
@@ -77,6 +103,50 @@ class MiscController: UIViewController{
     
 }
 
+extension UIView{
+    
+    func fillSuperview() {
+        anchor(top: superview?.topAnchor, leading: superview?.leadingAnchor, bottom: superview?.bottomAnchor, trailing: superview?.trailingAnchor)
+    }
+    
+    func anchorSize(to view: UIView) {
+        widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
+    }
+    func anchor(
+            top: NSLayoutYAxisAnchor?,
+            leading: NSLayoutXAxisAnchor?,
+            bottom: NSLayoutYAxisAnchor?,
+            trailing: NSLayoutXAxisAnchor?,
+            padding: UIEdgeInsets = .zero,
+            size: CGSize = .zero
+        ){
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        if let top = top {
+            topAnchor.constraint(equalTo: top,constant: padding.top).isActive = true
+        }
+        if let leading = leading {
+            leadingAnchor.constraint(equalTo: leading,constant: padding.left).isActive = true
+        }
+        if let bottom = bottom {
+            bottomAnchor.constraint(equalTo: bottom,constant: -padding.bottom).isActive = true
+        }
+        if let trailing = trailing {
+            trailingAnchor.constraint(equalTo: trailing,constant: -padding.right).isActive = true
+        }
+
+        
+        if size.width != 0 {
+            widthAnchor.constraint(equalToConstant: size.width).isActive = true
+        }
+
+        if size.height != 0 {
+            heightAnchor.constraint(equalToConstant: size.height).isActive = true
+        }
+        
+    }
+}
 
 extension MiscController: UITextViewDelegate {
     func textViewDidChange(_ textView: UITextView) {
