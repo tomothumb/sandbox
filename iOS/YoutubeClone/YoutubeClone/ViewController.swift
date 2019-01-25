368e7a920051942e62cff792cca698314b5b55e1
Tomoyuki Tsujimoto <tomo.tsujimoto@gmail.com>
2019-01-26 01:42:32 +0900

    iOS/YoutubeClone: init

---
+++ iOS/YoutubeClone/YoutubeClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YoutubeClone
+//
+//  Created by Tomoyuki Tsujimoto on 2019/01/26.
+//  Copyright © 2019年 Tomoyuki Tsujimoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
