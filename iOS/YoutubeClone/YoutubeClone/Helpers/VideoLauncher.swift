0b5971edf77725616dc2c1a32b94aba0997c3a1c
Tomoyuki Tsujimoto <tomo.tsujimoto@gmail.com>
2019-01-28 03:02:39 +0900

    iOS/YoutubeClone: video controller

--- iOS/YoutubeClone/YoutubeClone/Helpers/VideoLauncher.swift
+++ iOS/YoutubeClone/YoutubeClone/Helpers/VideoLauncher.swift
@@ -10,23 +10,100 @@ import UIKit
 import AVFoundation
 
 class VideoPlayerView: UIView {
+    
+    let activityIndicatorView: UIActivityIndicatorView = {
+        let aiv = UIActivityIndicatorView(style: UIActivityIndicatorView.Style.whiteLarge)
+        aiv.translatesAutoresizingMaskIntoConstraints = false
+        aiv.startAnimating()
+        return aiv
+    }()
+    
+    let pausePlayButton: UIButton = {
+        let button = UIButton(type: .system)
+        let image = UIImage(named: "search")
+        button.setImage(image, for: UIControl.State.normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.tintColor = .white
+        button.isHidden = true
+        button.addTarget(self, action: #selector(handlePausePlay), for: .touchUpInside)
+        return button
+    }()
+    
+    
+    var player:AVPlayer?
+    
+    var isPlaying = false
+
+    
+    // 再生・停止
+    @objc func handlePausePlay(){
+
+        if isPlaying {
+            player?.pause()
+            pausePlayButton.setImage(UIImage(named:"monitor"), for: .normal)
+        }else{
+            player?.play()
+            pausePlayButton.setImage(UIImage(named:"search"), for: .normal)
+        }
+        isPlaying = !isPlaying
+    }
+    
+    let controlsContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(white: 0, alpha: 1)
+//        addGestureRecognizer(UITapGestureRecognizer())
+        return view
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+
+        setupPlayerView()
+
+        controlsContainerView.frame = frame
+        addSubview(controlsContainerView)
+        
+        controlsContainerView.addSubview(activityIndicatorView)
+        activityIndicatorView.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        activityIndicatorView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        
+        controlsContainerView.addSubview(pausePlayButton)
+        pausePlayButton.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        pausePlayButton.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        pausePlayButton.widthAnchor.constraint(equalToConstant: 50).isActive = true
+        pausePlayButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
         
         backgroundColor = .black
-        
+    }
+    
+    // 動画ビュー
+    func setupPlayerView(){
         // Dummy Video
         let urlString = "https://firebasestorage.googleapis.com/v0/b/gameofchats-762ca.appspot.com/o/message_movies%2F12323439-9729-4941-BA07-2BAE970967C7.mov?alt=media&token=3e37a093-3bc8-410f-84d3-38332af9c726"
         
         if let url = URL(string: urlString){
-            let player = AVPlayer(url: url)
+            player = AVPlayer(url: url)
             let playerLayer = AVPlayerLayer(player: player)
             self.layer.addSublayer(playerLayer)
             playerLayer.frame = self.frame
             
-            player.play()
+            player?.play()
+            player?.addObserver(self, forKeyPath: "currentItem.loadedTimeRanges", options: .new, context: nil)
         }
     }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        
+        // 動画の準備ができたとき
+        if keyPath == "currentItem.loadedTimeRanges" {
+            print(change ?? 0)
+            activityIndicatorView.stopAnimating()
+            controlsContainerView.backgroundColor = .clear
+            pausePlayButton.isHidden = false
+            isPlaying = true
+        }
+    }
+
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
