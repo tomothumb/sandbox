f5c5fbe63b6e83f1443b7bb051cb691b2b74a7f7
Tomoyuki Tsujimoto <tomo.tsujimoto@gmail.com>
2019-02-03 14:07:25 +0900

    unity/book_beginner_ch05: timer and button

---
+++ unity/book_beginner_ch05/Assets/Click.js
@@ -0,0 +1,10 @@
+﻿#pragma strict
+
+ public function StopTimer(){
+ 
+       if(TimerCount.stop){
+         TimerCount.stop = false;
+       }else{
+         TimerCount.stop = true;
+        }
+     }
---
+++ unity/book_beginner_ch05/Assets/TimerCount.js
@@ -0,0 +1,24 @@
+﻿#pragma strict
+import UnityEngine.UI;
+
+private var timerText : Text;
+private var time : float;
+private var currentTime : int;
+public static var stop : boolean = false;
+
+
+function Start () {
+timerText = GetComponent.<Text>();
+
+}
+
+function Update () {
+if(stop){
+time += Time.deltaTime;
+currentTime = time;
+timerText.text = "Timer :" + currentTime;
+}
+}
+
+
+
