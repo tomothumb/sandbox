90e679a8a8e0bb8140d651d5803aa855c999124e
Tomoyuki Tsujimoto <tomo.tsujimoto@gmail.com>
2019-02-17 18:39:53 +0900

    iOS/Grid: drag animation

--- iOS/GridSample/GridSample/ViewController.swift
+++ iOS/GridSample/GridSample/ViewController.swift
@@ -10,11 +10,95 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let numViewPerRow = 20
+    var cells = [String: UIView]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let width = view.frame.width / CGFloat(numViewPerRow)
+
+        for j in 0...50 {
+            for i in 0...numViewPerRow {
+                let cellView = UIView()
+                cellView.backgroundColor = randomColor()
+                cellView.frame = CGRect(x: CGFloat(i) * width, y: CGFloat(j) * width, width: width, height: width)
+                cellView.layer.borderWidth = 0.25
+                cellView.layer.borderColor = UIColor.black.cgColor
+                view.addSubview(cellView)
+                
+                let key = "\(i)|\(j)"
+                cells[key] = cellView
+            }
+        }
+        
+        view.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(handlePan)))
+    }
+    
+    var selectedCell: UIView?
+    
+    @objc func handlePan(gesture: UIPanGestureRecognizer){
+        let location = gesture.location(in: view)
+//        print(location)
+        
+        let width = view.frame.width / CGFloat(numViewPerRow)
+
+        let i = Int(location.x / width)
+        let j = Int(location.y / width)
+        
+        let key = "\(i)|\(j)"
+        
+        guard let cellView = cells[key] else {return }
+        
+        if selectedCell != cellView {
+
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: UIView.AnimationOptions.curveEaseOut, animations: {
+                
+                self.selectedCell?.layer.transform = CATransform3DIdentity
+                
+            }, completion: nil)
+        }
+        
+        selectedCell = cellView
+        
+        view.bringSubviewToFront(cellView)
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: UIView.AnimationOptions.curveEaseOut, animations: {
+            
+            cellView.layer.transform = CATransform3DMakeScale(5,5,5)
+            
+        }, completion: nil)
+        
+        if gesture.state == .ended {
+            cellView.backgroundColor = .white
+            
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: UIView.AnimationOptions.curveEaseOut, animations: {
+                
+                self.selectedCell?.layer.transform = CATransform3DIdentity
+                
+            }, completion: nil)
+            
+        }
+        
+//        var loopCount = 0
+//        for subView in view.subviews {
+//            if subView.frame.contains(location) {
+//                subView.backgroundColor = .black
+//                print("loopCount:", loopCount)
+//            }
+//            loopCount += 1
+//        }
+        
+    }
+    
+    fileprivate func randomColor() -> UIColor {
+        let red = CGFloat(drand48())
+        let green = CGFloat(drand48())
+        let blue = CGFloat(drand48())
+        return UIColor(red: red, green: green, blue: blue, alpha: 1)
     }
 
+    
 
 }
 
