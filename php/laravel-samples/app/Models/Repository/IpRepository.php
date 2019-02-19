1494ecca93b34c3e21d8c290585b4d3084168513
Tomoyuki Tsujimoto <tomo.tsujimoto@gmail.com>
2019-02-20 00:05:30 +0900

    Laravel: IPv4 small changes

--- php/laravel-samples/app/Models/Repository/IpRepository.php
+++ php/laravel-samples/app/Models/Repository/IpRepository.php
@@ -13,11 +13,15 @@ class IpRepository
 
     public static function find(string $ip_address){
         $ip = Ip::where('ip_from', "=", IpUtil::ipv4ToInt($ip_address))->get();
+
         if($ip->count() == 0){
-            $ip = Ip::where('ip_from', "<=", IpUtil::ipv4ToInt($ip_address))
-                ->where('ip_to', ">=", IpUtil::ipv4ToInt($ip_address))
-                ->get();
+            // $ip1 = Ip::where('ip_from', "<=", IpUtil::ipv4ToInt($ip_address))->get();
+            // $ip2 = Ip::whereRaw('ip_from <= :from', ['from' => IpUtil::ipv4ToInt($ip_address)])->get();
+            $ip3 = Ip::whereRaw('ip_from <= '.IpUtil::ipv4ToInt($ip_address).' AND ip_to >= '.IpUtil::ipv4ToInt($ip_address))->get();
+            $ip = $ip3;
         }
+
+
         if($ip->count() > 0){
             return $ip;
         }
--- php/laravel-samples/routes/console.php
+++ php/laravel-samples/routes/console.php
@@ -21,28 +21,90 @@ Artisan::command('inspire', function () {
 })->describe('Display an inspiring quote');
 
 Artisan::command('ipv4crawl',function(){
-    $nextip = \App\Service\Whois\WhoisService::incrementIpv4Counter();
+
+    // 通常
+    $nextip = \App\Service\Whois\WhoisServiceUtil::incrementIpv4Counter();
 
     if($ip_addreses = IpRepository::find($nextip)){
         $ip_model = $ip_addreses->first();
-        $nextip = \App\Service\Whois\WhoisService::setIpv4Counter($ip_model->ip_to);
+        $nextip = \App\Service\Whois\WhoisServiceUtil::setIpv4Counter($ip_model->ip_to);
         echo "EXIST IP ADDRESS [IP RANGE FINISH]:{$nextip}\n";
     }else{
-//        $parser = WhoisService::detectWhoisServer($nextip);
-//        if($parser instanceof JpnicParser){
-            $ip_obj = WhoisService::addIp($nextip);
-            if($ip_obj->ip_to != "" ){
-                $nextip = \App\Service\Whois\WhoisService::setIpv4Counter($ip_obj->ip_to);
-            }
-//        }
+        $ip_obj = WhoisService::addIp($nextip);
+        if($ip_obj->ip_to != "" ){
+            $nextip = \App\Service\Whois\WhoisServiceUtil::setIpv4Counter($ip_obj->ip_to);
+        }
         echo "{$nextip}\n";
     }
+
+    // CSV
+    for($i=0;$i<100;$i++){
+        $ipv4 = getNextIpv4FromCsv();
+        if($ipv4 === false){ return; }
+        echo "{$ipv4}\n";
+        if(! WhoisService::hasIp($ipv4)){
+            $ip_obj = WhoisService::addIp($ipv4);
+            echo "DONE\n";
+            return;
+        }
+    }
+
+    echo "NO RESULT\n";
+
 });
 
 Artisan::command('ipv4whois {ipv4}',function($ipv4){
+    $ip_addreses = IpRepository::find($ipv4);
     if(! WhoisService::hasIp($ipv4)){
-        $parser = WhoisService::detectWhoisServer($ipv4);
         $ip_obj = WhoisService::addIp($ipv4);
     }
     echo "{$ipv4}\n";
 });
+
+function getNextIpv4FromCsv(){
+    $file_path = base_path('database/data/ipv4_crawl_list.txt');
+    // Read File
+    $lines = file($file_path);
+
+    try{
+        $ipv4 = trim($lines[0]);
+        // Update FIle
+        unset($lines[0]);
+        file_put_contents($file_path, $lines);
+        return $ipv4;
+
+    } catch (\Exception $e){
+        return false;
+    }
+
+}
+
+Artisan::command('ipv4crawlcsv',function(){
+
+    $ipv4 = getNextIpv4FromCsv();
+    echo "{$ipv4}\n";
+    if(! WhoisService::hasIp($ipv4)){
+        $ip_obj = WhoisService::addIp($ipv4);
+    }
+    echo "DONE\n";
+});
+
+Artisan::command('rdap',function(){
+    WhoisService::addIp('223.2.0.1');
+    return true;
+});
+
+
+Artisan::command('testtest',function(){
+    WhoisService::addIp('2.2.1.1'); // Ripe
+    WhoisService::addIp('197.200.1.1'); // AfriNic
+    WhoisService::addIp('181.100.0.16'); // LacNic
+
+    WhoisService::addIp('205.251.150.162');
+
+    return true;
+});
+
+Artisan::command('dbdump',function(){
+});
+
