586a70956f57c737489ee5bfe443005e3aed5f42
Tomoyuki Tsujimoto <tomo.tsujimoto@gmail.com>
2019-03-16 23:49:46 +0900

    javascript/stickyTOC: support resize

--- javascript/snippet/js/vendor/sticky_toc/jquery.sticky_toc.js
+++ javascript/snippet/js/vendor/sticky_toc/jquery.sticky_toc.js
@@ -1,9 +1,9 @@
 var StickyTOC = function(setting){
-    this.init(setting);
+    this.__init(setting);
 };
 
 // 初期設定
-StickyTOC.prototype.init = function(setting){
+StickyTOC.prototype.__init = function(setting){
 
     this.setting = {
         'selector': setting.selector ? setting.selector : '.sticky_toc',
@@ -11,10 +11,10 @@ StickyTOC.prototype.init = function(setting){
         'active_class': setting.active_class ? setting.selector : 'js-active',
         'navs': []
     };
+    this.__initClickEvent();
+};
 
-    var target_selector_str = this.setting.selector + ' ' + this.setting.selector_child;
-
-    var self = this;
+StickyTOC.prototype.__initClickEvent = function() {
     $(target_selector_str).each(function(){
         var data = $(this).data('side_nav_target');
         var $target = $('#' + data);
@@ -29,15 +29,27 @@ StickyTOC.prototype.init = function(setting){
         // イベント登録
         $(this).click(function(){
             // スムーズスクロール
-            $("html, body").animate({scrollTop: targetOffsetTop}, 500, "swing");
+            $("html, body").animate({scrollTop: self.setting.navs.offset}, 500, "swing");
             return false;
         })
 
     });
 };
 
+StickyTOC.prototype.__setPosition = function() {
+    var self = this;
+
+    tmp_nodelist = this.setting.navs;
+    tmp_node = Array.prototype.slice.call(tmp_nodelist,0);
+
+    // this.setting.navs.forEach(function (value, index, array) {
+    tmp_node.forEach(function (value, index, array) {
+        self.setting.navs[index].offset = Math.floor(value.$target.offset().top);
+    });
+};
+
 // 監視
-StickyTOC.prototype.watch = function(){
+StickyTOC.prototype.__watch = function(){
     var self = this;
 
     var tmp_nodelist;
@@ -62,7 +74,20 @@ StickyTOC.prototype.watch = function(){
 StickyTOC.prototype.run = function(){
     var self = this;
     $(window).on("scroll",function(){ // スクロール毎にイベントが発火します。
-        self.watch();
+        self.__watch();
+    });
+
+
+    // リサイズ
+    var resizeTimer;
+    var interval = Math.floor(1000 / 60 * 10);
+    window.addEventListener('resize', function (event) {
+        if (resizeTimer !== false) {
+            clearTimeout(resizeTimer);
+        }
+        resizeTimer = setTimeout(function () {
+            self.__setPosition();
+        }, interval);
     });
 
 };
