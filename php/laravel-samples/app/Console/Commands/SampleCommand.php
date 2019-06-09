b2c9d2af1071fb407fae898dd3766f1f8b632e2e
Tomoyuki Tsujimoto <tomo.tsujimoto@gmail.com>
2019-06-10 05:12:59 +0900

    laravel: interactive cli sample

---
+++ php/laravel-samples/app/Console/Commands/SampleCommand.php
@@ -0,0 +1,161 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class SampleCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'sample:sample';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->info('start');
+        $this->line('line');
+
+        $models = [];
+        $col = [
+            'key' => null,
+            'type' => null,
+            'length' => null,
+            'nullable' => null,
+            'comment' => null,
+        ];
+        $columns = [];
+        $model = $this->ask('Model名を入力してください');
+
+        $this->info('Column`ID` は作成されます');
+        $columns = $this->interactive();
+
+        $validator = \Validator::make([
+//            'first_name' => $firstName,
+//            'last_name' => $lastName,
+//            'email' => $email,
+//            'password' => $password,
+        ], [
+//            'first_name' => ['required'],
+//            'last_name' => ['required'],
+//            'email' => ['required', 'email', 'unique:staff,email'],
+//            'password' => ['required', 'min:8'],
+        ]);
+
+        if ($validator->fails()) {
+            $this->info('Staff User not created. See error messages below:');
+            foreach ($validator->errors()->all() as $error) {
+                $this->error($error);
+            }
+            $columns = $this->interactive();
+
+        }
+
+        $this->line('line');
+        foreach ($columns as $column){
+            $this->line('line');
+            foreach ($column as $key => $val){
+                $this->info(sprintf("%s:%s",$key,$val));
+            }
+
+        }
+
+        $makeModel_flg = $this->confirm('Make Model File?', 1);
+        $makeMigration_flg = $this->confirm('Make Migration File?', 1);
+        $makeFactory_flg = $this->confirm('Make Factory File?', 1);
+        $makeSeeder_flg = $this->confirm('Make Seeder File?', 1);
+
+        if($makeModel_flg){
+            $this->makeModel($model, $column);
+        }
+        if ($makeMigration_flg){
+            $this->makeMigration($model, $column);
+        }
+        if ($makeFactory_flg){
+            $this->makeFactory($model, $column);
+        }
+        if ($makeSeeder_flg){
+            $this->makeSeeder($model, $column);
+        }
+
+        $this->line('line');
+        $this->info('end');
+    }
+
+    protected function interactive(){
+
+        $continue = true;
+        $index = 0;
+        while ($continue){
+            $column_key = $this->ask('Table Column');
+            $column_type = $this->anticipate('Type [string|text|integer|date]', ['string', 'text', 'integer', 'date']);
+            $column_length = null;
+            if($column_type == 'string'){
+                $column_length = $this->ask('Length', 191);
+            }
+            $column_unsigned = null;
+            if($column_type == 'integer'){
+                $column_unsigned = $this->confirm('unsigned', 1);
+            }
+
+            $column_nullable = $this->confirm('nullable', 1);
+            $column_comment = $this->ask('Comment', false);
+
+            $columns[$index] = [
+                'key' => $column_key,
+                'type' => $column_type,
+                'length' => $column_length,
+                'nullable' => $column_nullable,
+                'unsigned' => $column_unsigned,
+                'comment' => $column_comment,
+            ];
+
+            $index++;
+            $continue = $this->confirm('カラムを追加しますか？');
+        }
+
+        return $columns;
+
+    }
+
+    protected function makeModel($model, $column){
+        $this->call('command:called', ['--path' => 'call']);
+    }
+
+    protected function makeMigration($model, $column){
+
+    }
+
+    protected function makeFactory($model, $column){
+
+    }
+
+    protected function makeSeeder($model, $column){
+
+    }
+
+}
