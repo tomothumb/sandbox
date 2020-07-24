171b819847c668ef2d753af569e1c2f4e7ab038d
Tomoyuki Tsujimoto <tomo.tsujimoto@gmail.com>
2020-07-24 14:32:23 +0900

    react-spring: init

---
+++ javascript/react/react-spring/src/App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+// import logo from './logo.svg';
+import {Spring} from './Components/Spring';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Spring />
+    </div>
+  );
+}
+
+export default App;
