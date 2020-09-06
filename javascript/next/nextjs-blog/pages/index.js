855c762b31acd98d49aff9b6b29dedc4d75fee05
Tomoyuki Tsujimoto <tomo.tsujimoto@gmail.com>
2020-09-06 21:51:05 +0900

    nextjs: dynamic route

--- javascript/next/nextjs-blog/pages/index.js
+++ javascript/next/nextjs-blog/pages/index.js
@@ -2,8 +2,11 @@ import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import Alert from "../components/alert";
+import { getSortedPostsData } from '../lib/posts'
+import Link from 'next/link'
+import Date from '../components/date'
 
-export default function Home() {
+export default function Home({allPostsData}) {
   return (
       <Layout home>
         <Head>
@@ -16,7 +19,32 @@ export default function Home() {
             (This is a sample website - you’ll be building a site like this on{' '}
             <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
           </p>
+
+            <ul className={utilStyles.list}>
+                {allPostsData.map(({ id, date, title }) => (
+                    <li className={utilStyles.listItem} key={id}>
+                        <Link href="/posts/[id]" as={`/posts/${id}`}>
+                            <a>{title} - {id}</a>
+                        </Link>
+                        <br />
+                        <small className={utilStyles.lightText}>
+                            <Date dateString={date} />
+                        </small>
+                    </li>
+
+                    ))}
+            </ul>
         </section>
       </Layout>
   )
+}
+
+
+export async function getStaticProps() {
+    const allPostsData = getSortedPostsData()
+    return {
+        props: {
+            allPostsData
+        }
+    }
 }
