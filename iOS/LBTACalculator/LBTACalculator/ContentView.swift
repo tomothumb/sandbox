9bc434ad4b1a3f51da7aceb65856095c885da2d4
Tomoyuki Tsujimoto <tomo.tsujimoto@gmail.com>
2020-11-15 01:28:40 +0900

    SwiftUI/calculator: init
    https://www.youtube.com/watch?v=ULEFrRSPXFE

---
+++ iOS/LBTACalculator/LBTACalculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LBTACalculator
+//
+//  Created by Tomoyuki Tsujimoto on 2020/11/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS/LBTACalculator/LBTACalculator/LBTACalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  LBTACalculatorApp.swift
+//  LBTACalculator
+//
+//  Created by Tomoyuki Tsujimoto on 2020/11/15.
+//
+
+import SwiftUI
+
+@main
+struct LBTACalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
